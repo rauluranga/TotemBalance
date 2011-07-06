824678aedbaa695c4119b5232602c169848b8444
Raúl Uranga <ruranga@gmail.com>
2011-07-06 14:12:01 -0500

    Global Class is setup as Totem

---
+++ Classes/Goal.h
@@ -0,0 +1,16 @@
+//
+//  Goal.h
+//  TotemBalance
+//
+//  Created by Raúl Uranga on 7/6/11.
+//  Copyright 2011 GrupoW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Goal : NSObject {
+
+}
+
+@end
---
+++ Classes/Goal.m
@@ -0,0 +1,14 @@
+//
+//  Goal.m
+//  TotemBalance
+//
+//  Created by Raúl Uranga on 7/6/11.
+//  Copyright 2011 GrupoW. All rights reserved.
+//
+
+#import "Goal.h"
+
+
+@implementation Goal
+
+@end
